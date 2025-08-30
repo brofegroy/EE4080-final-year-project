420a9e5e8c1cc6b2939d9aa1751031896642bdf8
Cher Yuan <coy001@e.ntu.edu.sg>
2025-08-30 16:45:47 +0800

    commiting something so the repository isnt empty

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # EE4080-final-year-project
+
+this FYP folder is to be put in the same directory as the 'examples' folder in dwm3000-master to work properly
+
+constants declared with 'E002F9B9' prefix to avoid clashes, this value is the first 8 hex of the sha256 hash of 'EE4080', the course code i am doing this project for.
---
+++ common/ieee802154_mac_constants.h
@@ -0,0 +1,69 @@
+#ifndef IEEE802154_MAC_CONSTANTS_H
+#define IEEE802154_MAC_CONSTANTS_H
+
+#ifdef namespace_E002F9B9
+#error "namespace E002F9B9" is already used 
+#endif //namespace_E002F9B9
+#define namespace_E002F9B9
+
+#include <stdint.h>
+
+// how are variable named:
+// E002F9B9_MAC__<octet name>__<bit name>_<value>
+
+#pragma region Frame Control Field
+// ###Frame Control Field (FCF), IEEE pg 162
+// Frame type (bits 0–2)
+#define E002F9B9_MAC__FCF__FRAME_TYPE_BEACON        0b000
+#define E002F9B9_MAC__FCF__FRAME_TYPE_DATA          0b001
+#define E002F9B9_MAC__FCF__FRAME_TYPE_ACK           0b010
+#define E002F9B9_MAC__FCF__FRAME_TYPE_MAC_CMD       0b011
+#define E002F9B9_MAC__FCF__FRAME_TYPE_RESERVED      0b100
+#define E002F9B9_MAC__FCF__FRAME_TYPE_MULTIPURPOSE  0b101
+#define E002F9B9_MAC__FCF__FRAME_TYPE_FRAGMENT      0b110
+#define E002F9B9_MAC__FCF__FRAME_TYPE_EXTENDED      0b111
+
+// Security Enabled (bit 3)
+#define E002F9B9_MAC__FCF__SECURITY_ENABLED_1    (1 << 3)
+#define E002F9B9_MAC__FCF__SECURITY_ENABLED_0    0b0
+
+// Frame Pending (bit 4)
+#define E002F9B9_MAC__FCF__FRAME_PENDING_1       (1 << 4)
+#define E002F9B9_MAC__FCF__FRAME_PENDING_0       0b0
+
+// Acknowledgment Request (bit 5)
+#define E002F9B9_MAC__FCF__ACK_REQUEST_1         (1 << 5)
+#define E002F9B9_MAC__FCF__ACK_REQUEST_0         0b0
+
+// PAN ID Compression (bit 6)
+#define E002F9B9_MAC__FCF__PAN_ID_COMPRESSION_1  (1 << 6)
+#define E002F9B9_MAC__FCF__PAN_ID_COMPRESSION_0  0b0
+
+// Bit 7 is reserved
+
+// Sequence Number Suppression (bit 8)
+#define E002F9B9_MAC__FCF__SEQUENCE_SUPPRESS_1   (1 << 8)
+#define E002F9B9_MAC__FCF__SEQUENCE_SUPPRESS_0   0b0
+
+// Information Elements Present (bit 9)
+#define E002F9B9_MAC__FCF__IE_PRESENT_1          (1 << 9)
+#define E002F9B9_MAC__FCF__IE_PRESENT_0          0b0
+
+// Destination Addressing Modes (bits 10-11)
+#define E002F9B9_MAC__FCF__DEST_ADDR_MODE_NONE_00      (0b00 << 10)
+#define E002F9B9_MAC__FCF__DEST_ADDR_MODE_SHORT_10     (0b10 << 10)
+#define E002F9B9_MAC__FCF__DEST_ADDR_MODE_EXTENDED_11  (0b11 << 10)
+
+// Frame version (bits 12–13)
+#define E002F9B9_MAC__FCF__FRAME_VERSION_2003_00     (0b00 << 12)
+#define E002F9B9_MAC__FCF__FRAME_VERSION_2006_01     (0b01 << 12)
+#define E002F9B9_MAC__FCF__FRAME_VERSION_2020_10     (0b10 << 12)
+#define E002F9B9_MAC__FCF__FRAME_VERSION_RESERVED_11 (0b11 << 12)
+
+// Source Addressing Modes (bits 14-15)
+#define E002F9B9_MAC__FCF__SRC_ADDR_MODE_NONE_00       (0b00 << 14)
+#define E002F9B9_MAC__FCF__SRC_ADDR_MODE_SHORT_01      (0b10 << 14)
+#define E002F9B9_MAC__FCF__SRC_ADDR_MODE_EXTENDED_11   (0b11 << 14)
+
+#pragma endregion Frame Control Field
+
