b06e72735e7777a8382ed1dde76d2d93c2d48af8
Cher Yuan <coy001@e.ntu.edu.sg>
2025-09-01 19:02:01 +0800

    not tested

---
+++ FYP_1_beaconframe_test1/FYP_1_beaconframe_test1.c
@@ -0,0 +1,248 @@
+#include <deca_device_api.h>
+#include <deca_regs.h>
+#include <deca_spi.h>
+#include <port.h>
+#include <shared_defines.h>
+
+//zephyr includes
+#if __has_include(<zephyr/kernel.h>)
+// @suppress "Cannot open source file"
+#include <zephyr/kernel.h>
+#endif
+#include <zephyr/sys/printk.h>
+
+#define APP_NAME "FYP_1_beaconframe_test1"
+
+#define LOG_LEVEL 3
+#include <zephyr/logging/log.h>
+LOG_MODULE_REGISTER(APP_NAME);
+//-----------------
+#include "../common/ieee802154_mac_constants.h"
+
+/*
+ASSUMPTIONS
+standard beacon format, not enhanced IEEE pg169
+security enabled = false
+frame pending = false
+AR = false
+no IE for now as no additional information needs to be carried at lin layer for now
+assume single PAN
+using short addresses for this implmentation, up to 65565 devices supported
+there is no hiher layer than the MC layer currently so beacon payload will be empty
+assume address of source/beacon/coordinator are 0
+assume broadcast address is 0
+Omit superframe specification and GTS in this example for simplicity, also because is not manditory in version 0b10
+beacon frame taken as ALOHA, always clear, no need CCA
+
+Current version:
+assumes no Reduced function device (RFD) will request GTS in this version, 
+only generating the base beacon frame.
+*/
+
+/* Default communication configuration. We use default non-STS DW mode. */
+static dwt_config_t config = {
+    5,               /* Channel number. */
+    DWT_PLEN_128,    /* Preamble length. Used in TX only. */
+    DWT_PAC8,        /* Preamble acquisition chunk size. Used in RX only. */
+    9,               /* TX preamble code. Used in TX only. */
+    9,               /* RX preamble code. Used in RX only. */
+    1,               /* 0 to use standard 8 symbol SFD, 
+                      *   1 to use non-standard 8 symbol, 
+                      *   2 for non-standard 16 symbol SFD and 
+                      *   3 for 4z 8 symbol SDF type */
+    DWT_BR_6M8,      /* Data rate. */
+    DWT_PHRMODE_STD, /* PHY header mode. */
+    DWT_PHRRATE_STD, /* PHY header rate. */
+    (129 + 8 - 8),   /* SFD timeout (preamble length + 1 + SFD length - PAC size). 
+                      *  Used in RX only. */
+    DWT_STS_MODE_OFF, /* STS disabled */
+    DWT_STS_LEN_64,/* STS length see allowed values in Enum dwt_sts_lengths_e */
+    DWT_PDOA_M0      /* PDOA mode off */
+};
+
+/* Index to access to sequence number of the blink frame in the tx_msg array. */
+#define BLINK_FRAME_SN_IDX 1
+
+/* The real length that is going to be transmitted */
+#define FRAME_LENGTH    (sizeof(tx_msg)+FCS_LEN) 
+
+/* Inter-frame delay period, in milliseconds.
+ * this example will try to transmit a frame every 100 ms*/
+#define TX_DELAY_MS 100
+
+// no cca
+// /* Initial backoff period when failed to transmit a frame due to preamble detection. */
+// #define INITIAL_BACKOFF_PERIOD 400 /* This constant would normally smaller (e.g. 1ms),
+//                                     * however here it is set to 400 ms so that
+//                                     * user can see (on Zephyr RTT console) the report 
+//                                     * that the CCA detects a preamble on the air occasionally,
+//                                     * and is doing a TX back-off.
+//                                     */
+
+int tx_sleep_period; /* Sleep period until the next TX attempt */
+
+// beacon frame taken as ALOHA, always clear, no need CCA
+// /* Next backoff in the event of busy channel detection by this pseudo CCA algorithm */
+// int next_backoff_interval = INITIAL_BACKOFF_PERIOD;
+
+/* holds copy of status register */
+uint32_t status_reg = 0;
+uint32_t status_regh = 0; /* holds the high 32 bits of SYS_STATUS_HI */
+
+/* Values for the PG_DELAY and TX_POWER registers reflect the bandwidth and 
+ * power of the spectrum at the current temperature. 
+ * These values can be calibrated prior to taking reference measurements. 
+ * See NOTE 3 below. */
+extern dwt_txconfig_t txconfig_options;
+
+int app_main(void)
+{
+    /* Display application name. */
+    LOG_INF(APP_NAME);
+
+    /* Configure SPI rate, DW3000 supports up to 38 MHz */
+    port_set_dw_ic_spi_fastrate();
+
+    /* Reset DW IC */
+    reset_DWIC(); /* Target specific drive of RSTn line into DW IC low for a period. */
+
+    /* Time needed for DW3000 to start up (transition from INIT_RC to IDLE_RC,
+     * or could wait for SPIRDY event) */
+    Sleep(2); 
+
+    /* Need to make sure DW IC is in IDLE_RC before proceeding */
+    while (!dwt_checkidlerc()) { /* spin */ };
+
+    if (dwt_initialise(DWT_DW_INIT) == DWT_ERROR)
+    {
+        LOG_ERR("INIT FAILED");
+        while (1) { /* spin */ };
+    }
+
+    /* Enabling LEDs here for debug so that for each TX the D1 LED will flash
+     * on DW3000 red eval-shield boards. */
+    dwt_setleds(DWT_LEDS_ENABLE | DWT_LEDS_INIT_BLINK) ;
+
+    /* Configure DW IC. See NOTE 7 below. */
+    /* If the dwt_configure returns DWT_ERROR either the PLL or RX calibration
+     * has failed the host should reset the device */
+    if (dwt_configure(&config)) 
+    {
+        LOG_ERR("CONFIG FAILED");
+        while (1) { /* spin */ };
+    }
+
+    /* Configure the TX spectrum parameters (power, PG delay and PG count) */
+    dwt_configuretxrf(&txconfig_options);
+
+    /* Configure preamble timeout to 3 PACs; if no preamble detected in this 
+     * time we assume channel is clear. See NOTE 4*/
+    dwt_setpreambledetecttimeout(3);
+
+    /* The frame sent in this example is an 802.15.4 standard blink. 
+    * It is a 12-byte frame composed of the following fields:
+    *     - byte 0: frame type (0x00 for a beacon). -page 162 of IEEE802
+    *     - byte 1: sequence number, incremented for each new frame.
+    *     - byte 10/11: frame check-sum, automatically set/added by DW3000.  */
+    // static uint8_t tx_msg[] = {beacon_fcf, 0, 'D', 'E', 'C', 'A', 'W', 'A', 'V', 'E'};
+
+    /* Loop forever sending BEACON frames periodically. */
+    while(1)
+    {
+        ///Forming the frame to be sent
+
+        //Forming the frame control field of the frame
+        uint16_t beacon_frame_control_field = 
+            E002F9B9_MAC__FCF__FRAME_TYPE_BEACON       | // beacon frame always 0
+            E002F9B9_MAC__FCF__SECURITY_ENABLED_0      | // assume no security
+            E002F9B9_MAC__FCF__FRAME_PENDING_0         | // beacon has pending data from recepient
+            E002F9B9_MAC__FCF__ACK_REQUEST_0           | // beacon frames not require ack
+            E002F9B9_MAC__FCF__PAN_ID_COMPRESSION_1    | // assumed single PAN for this implementation, so no need pan ID
+            E002F9B9_MAC__FCF__SEQUENCE_SUPPRESS_0     | // reccomended to have sequence number for duplicate detection/missing frames
+            E002F9B9_MAC__FCF__IE_PRESENT_0            | // no need for IE in this implementation
+            E002F9B9_MAC__FCF__DEST_ADDR_MODE_SHORT_10 | // beacon frames not require destination
+            E002F9B9_MAC__FCF__FRAME_VERSION_2020_10   | // self explanatory
+            E002F9B9_MAC__FCF__SRC_ADDR_MODE_SHORT_01;   // can accomodate 65535 devices
+        
+        //Forming the initial Sequence number of the frame
+        uint8_t beacon_seq_num_field = 0;
+
+        //Addressing Field 
+        uint16_t beacon_addressing_field_source_address      = 0;      //assume address of beacon is 0
+        uint16_t beacon_addressing_field_destination_address = 0xFFFF; //assume address of broadcast is 0xFFFF
+
+        //Security field omitted
+
+        int frame_length = 
+            sizeof(beacon_frame_control_field) +
+            sizeof(beacon_seq_num_field) +
+            sizeof(beacon_addressing_field_source_address) +
+            //no security header
+            //no superframe specification
+            //no GTS info
+            //no pending address field
+            //no IE field
+            //no beacon payload
+            FCS_LEN;
+
+        static uint8_t tx_msg[] = {
+            (uint8_t)(beacon_frame_control_field & 0xFF),                        // FCF LSB
+            (uint8_t)((beacon_frame_control_field >> 8) & 0xFF),                 // FCF MSB
+            beacon_seq_num_field,                                                // Sequence number
+            (uint8_t)(beacon_addressing_field_source_address & 0xFF),            // Src addr LSB
+            (uint8_t)((beacon_addressing_field_source_address >> 8) & 0xFF),     // Src addr MSB
+            (uint8_t)(beacon_addressing_field_destination_address & 0xFF),       // Dest addr LSB
+            (uint8_t)((beacon_addressing_field_destination_address >> 8) & 0xFF) // Dest addr MSB
+        };
+        
+        
+        /* Write frame data to DW3000 and prepare transmission..*/
+        dwt_writetxdata(frame_length - FCS_LEN, tx_msg, 0); /* Zero offset in TX buffer. */
+        dwt_writetxfctrl(FRAME_LENGTH, 0, 0); /* Zero offset in TX buffer, no ranging. */
+
+        /* Start transmission with CCA. The transmission will only start once 
+         * there is no preamble detected within 3 PACs as defined above
+         * e.g. once we get the preamble timeout or TX will be canceled if a 
+         * preamble is detected. */
+        // dwt_starttx(DWT_START_TX_CCA);
+
+        // /* Poll DW3000 until either TX complete or CCA_FAIL. */
+        // while (!((status_reg = dwt_read32bitreg(SYS_STATUS_ID)) & SYS_STATUS_TXFRS_BIT_MASK))
+        // {
+        //     if ((status_regh = dwt_read32bitreg(SYS_STATUS_HI_ID))& SYS_STATUS_HI_CCA_FAIL_BIT_MASK) {
+        //         break;
+        //     }
+        // }
+
+        if (status_reg & SYS_STATUS_TXFRS_BIT_MASK)
+        {
+            tx_sleep_period = TX_DELAY_MS; /* sent a frame - set interframe period */
+            // next_backoff_interval = INITIAL_BACKOFF_PERIOD; /* set initial backoff period */
+
+            /* Increment the blink frame sequence number (modulo 256). */
+            tx_msg[2]++; // increase seq number
+
+            /* Reflect frame number */
+            LOG_INF("frame: %d", (int) tx_msg[2]);
+        }
+        else
+        {
+            /* If DW IC detected the preamble, device will be in IDLE */
+            tx_sleep_period = next_backoff_interval; /* set the TX sleep period */
+
+            next_backoff_interval++; /* If failed to transmit, increase backoff 
+                                      * and try again.
+                                      * In a real implementation the back-off 
+                                      * is typically a randomised period
+                                      * whose range is an exponentially related 
+                                      * to the number of successive failures.
+                                      * See https://en.wikipedia.org/wiki/Exponential_backoff */
+        }
+
+        /* Clear TX frame sent event. */
+        dwt_write32bitreg(SYS_STATUS_ID, SYS_STATUS_TXFRS_BIT_MASK);
+
+        /* Execute a delay between transmissions. */
+        Sleep(tx_sleep_period);
+    }
+}
--- common/ieee802154_mac_constants.h
+++ common/ieee802154_mac_constants.h
@@ -11,59 +11,128 @@
 // how are variable named:
 // E002F9B9_MAC__<octet name>__<bit name>_<value>
 
-#pragma region Frame Control Field
-// ###Frame Control Field (FCF), IEEE pg 162
-// Frame type (bits 0–2)
-#define E002F9B9_MAC__FCF__FRAME_TYPE_BEACON        0b000
-#define E002F9B9_MAC__FCF__FRAME_TYPE_DATA          0b001
-#define E002F9B9_MAC__FCF__FRAME_TYPE_ACK           0b010
-#define E002F9B9_MAC__FCF__FRAME_TYPE_MAC_CMD       0b011
-#define E002F9B9_MAC__FCF__FRAME_TYPE_RESERVED      0b100
-#define E002F9B9_MAC__FCF__FRAME_TYPE_MULTIPURPOSE  0b101
-#define E002F9B9_MAC__FCF__FRAME_TYPE_FRAGMENT      0b110
-#define E002F9B9_MAC__FCF__FRAME_TYPE_EXTENDED      0b111
+#pragma region Frame Control Field //IEEE pg 162
+//---------------------------------------------------------------
 
-// Security Enabled (bit 3)
-#define E002F9B9_MAC__FCF__SECURITY_ENABLED_1    (1 << 3)
-#define E002F9B9_MAC__FCF__SECURITY_ENABLED_0    0b0
+    // Frame type (bits 0-2)
+    #define E002F9B9_MAC__FCF__FRAME_TYPE_BEACON        0b000
+    #define E002F9B9_MAC__FCF__FRAME_TYPE_DATA          0b001
+    #define E002F9B9_MAC__FCF__FRAME_TYPE_ACK           0b010
+    #define E002F9B9_MAC__FCF__FRAME_TYPE_MAC_CMD       0b011
+    #define E002F9B9_MAC__FCF__FRAME_TYPE_RESERVED      0b100
+    #define E002F9B9_MAC__FCF__FRAME_TYPE_MULTIPURPOSE  0b101
+    #define E002F9B9_MAC__FCF__FRAME_TYPE_FRAGMENT      0b110
+    #define E002F9B9_MAC__FCF__FRAME_TYPE_EXTENDED      0b111
 
-// Frame Pending (bit 4)
-#define E002F9B9_MAC__FCF__FRAME_PENDING_1       (1 << 4)
-#define E002F9B9_MAC__FCF__FRAME_PENDING_0       0b0
+    // Security Enabled (bit 3)
+    #define E002F9B9_MAC__FCF__SECURITY_ENABLED_1    (1 << 3)
+    #define E002F9B9_MAC__FCF__SECURITY_ENABLED_0    0b0
 
-// Acknowledgment Request (bit 5)
-#define E002F9B9_MAC__FCF__ACK_REQUEST_1         (1 << 5)
-#define E002F9B9_MAC__FCF__ACK_REQUEST_0         0b0
+    // Frame Pending (bit 4)
+    #define E002F9B9_MAC__FCF__FRAME_PENDING_1       (1 << 4)
+    #define E002F9B9_MAC__FCF__FRAME_PENDING_0       0b0
 
-// PAN ID Compression (bit 6)
-#define E002F9B9_MAC__FCF__PAN_ID_COMPRESSION_1  (1 << 6)
-#define E002F9B9_MAC__FCF__PAN_ID_COMPRESSION_0  0b0
+    // Acknowledgment Request (bit 5)
+    #define E002F9B9_MAC__FCF__ACK_REQUEST_1         (1 << 5)
+    #define E002F9B9_MAC__FCF__ACK_REQUEST_0         0b0
 
-// Bit 7 is reserved
+    // PAN ID Compression (bit 6)
+    #define E002F9B9_MAC__FCF__PAN_ID_COMPRESSION_1  (1 << 6)
+    #define E002F9B9_MAC__FCF__PAN_ID_COMPRESSION_0  0b0
 
-// Sequence Number Suppression (bit 8)
-#define E002F9B9_MAC__FCF__SEQUENCE_SUPPRESS_1   (1 << 8)
-#define E002F9B9_MAC__FCF__SEQUENCE_SUPPRESS_0   0b0
+    // Bit 7 is reserved
 
-// Information Elements Present (bit 9)
-#define E002F9B9_MAC__FCF__IE_PRESENT_1          (1 << 9)
-#define E002F9B9_MAC__FCF__IE_PRESENT_0          0b0
+    // Sequence Number Suppression (bit 8)
+    #define E002F9B9_MAC__FCF__SEQUENCE_SUPPRESS_1   (1 << 8)
+    #define E002F9B9_MAC__FCF__SEQUENCE_SUPPRESS_0   0b0
 
-// Destination Addressing Modes (bits 10-11)
-#define E002F9B9_MAC__FCF__DEST_ADDR_MODE_NONE_00      (0b00 << 10)
-#define E002F9B9_MAC__FCF__DEST_ADDR_MODE_SHORT_10     (0b10 << 10)
-#define E002F9B9_MAC__FCF__DEST_ADDR_MODE_EXTENDED_11  (0b11 << 10)
+    // Information Elements Present (bit 9)
+    #define E002F9B9_MAC__FCF__IE_PRESENT_1          (1 << 9)
+    #define E002F9B9_MAC__FCF__IE_PRESENT_0          0b0
 
-// Frame version (bits 12–13)
-#define E002F9B9_MAC__FCF__FRAME_VERSION_2003_00     (0b00 << 12)
-#define E002F9B9_MAC__FCF__FRAME_VERSION_2006_01     (0b01 << 12)
-#define E002F9B9_MAC__FCF__FRAME_VERSION_2020_10     (0b10 << 12)
-#define E002F9B9_MAC__FCF__FRAME_VERSION_RESERVED_11 (0b11 << 12)
+    // Destination Addressing Modes (bits 10-11)
+    #define E002F9B9_MAC__FCF__DEST_ADDR_MODE_NONE_00      (0b00 << 10)
+    #define E002F9B9_MAC__FCF__DEST_ADDR_MODE_SHORT_10     (0b10 << 10)
+    #define E002F9B9_MAC__FCF__DEST_ADDR_MODE_EXTENDED_11  (0b11 << 10)
 
-// Source Addressing Modes (bits 14-15)
-#define E002F9B9_MAC__FCF__SRC_ADDR_MODE_NONE_00       (0b00 << 14)
-#define E002F9B9_MAC__FCF__SRC_ADDR_MODE_SHORT_01      (0b10 << 14)
-#define E002F9B9_MAC__FCF__SRC_ADDR_MODE_EXTENDED_11   (0b11 << 14)
+    // Frame version (bits 12-13)
+    #define E002F9B9_MAC__FCF__FRAME_VERSION_2003_00     (0b00 << 12)
+    #define E002F9B9_MAC__FCF__FRAME_VERSION_2006_01     (0b01 << 12)
+    #define E002F9B9_MAC__FCF__FRAME_VERSION_2020_10     (0b10 << 12)
+    #define E002F9B9_MAC__FCF__FRAME_VERSION_RESERVED_11 (0b11 << 12)
 
+    // Source Addressing Modes (bits 14-15)
+    #define E002F9B9_MAC__FCF__SRC_ADDR_MODE_NONE_00       (0b00 << 14)
+    #define E002F9B9_MAC__FCF__SRC_ADDR_MODE_SHORT_01      (0b10 << 14)
+    #define E002F9B9_MAC__FCF__SRC_ADDR_MODE_EXTENDED_11   (0b11 << 14)
+//----------------------------------------------------------------------------------
 #pragma endregion Frame Control Field
 
+#pragma region Beacon Superframe Specification Field //IEEE pg 171
+//--------------------------------------------------------
+    /// Beacon Superframe Specifiation Field, Beacon Order (bits 0-3)
+    #define E002F9B9_MAC__BSSF__BEACON_ORDER(n) \
+    (__builtin_constant_p(n) ? \
+        (static_assert((n) >= 0 && (n) <= 15, "Beacon Order must be 0-15"), \
+            (n << 0)) : \
+        (assert((n) >= 0 && (n) <= 15), \
+            (n << 0)))
+
+    /// Beacon Superframe Specifiation Field, Superframe Order (bits 4-7)
+    #define E002F9B9_MAC__BSSF__SUPERFRAME_ORDER(n) \
+    (__builtin_constant_p(n) ? \
+        (static_assert((n) >= 0 && (n) <= 15, "Superframe Order must be 0-15"), \
+            (n << 4)) : \
+        (assert((n) >= 0 && (n) <= 15), \
+            (n << 4)))
+
+    /// Beacon Superframe Specifiation Field, Final CAP Slot (bits 8-11)
+    #define E002F9B9_MAC__BSSF__FINAL_CAP_SLOT(n) \
+    (__builtin_constant_p(n) ? \
+        (static_assert((n) >= 0 && (n) <= 15, "Final CAP Slot must be 0-15"), \
+            (n << 8)) : \
+        (assert((n) >= 0 && (n) <= 15), \
+            (n << 8)))
+
+    // Battery Life Extension (bit 12)
+    #define E002F9B9_MAC__BSSF__BATTERY_LIFE_EXTENSION_1 (1 << 12)
+    #define E002F9B9_MAC__BSSF__BATTERY_LIFE_EXTENSION_0 0
+
+    // Bit 13 is Reserved
+
+    // PAN Coordinator (bit 14)
+    #define E002F9B9_MAC__BSSF__IS_PAN_COORDINATOR_1 (1 << 14)
+    #define E002F9B9_MAC__BSSF__IS_PAN_COORDINATOR_0 0
+
+    // Association Permit (bit 15)
+    #define E002F9B9_MAC__BSSF__ASSOCIATION_PERMIT_1 (1 << 15)
+    #define E002F9B9_MAC__BSSF__ASSOCIATION_PERMIT_0 0
+//--------------------------------------------------------
+#pragma endregion Beacon Superframe Specification
+
+#pragma region Beacon Pending Address Specification Field //IEEE pg 172
+//--------------------------------------------------------
+    /// Beacon Pending Address Specification Field,
+    /// Number of Short Addresses Pending (bits 0-2)
+    #define E002F9B9_MAC__BPASF__SHORT_ADDRESSES_PENDING_COUNT(n) \
+        (__builtin_constant_p(n) ? \
+            (static_assert((n) >= 0 && (n) <= 7, "Number of Short Addresses must be in range(0-7)"), \
+                (n << 0)) : \
+            (assert((n) >= 0 && (n) <= 7), \
+                (n << 0)))
+
+    // Bit 3 is Reserved
+
+    /// Beacon Pending Address Specification Field,
+    /// Number of Long Addresses Pending (bits 4-6)
+    #define E002F9B9_MAC__BPASF__LONG_ADDRESSES_PENDING_COUNT(n) \
+    (__builtin_constant_p(n) ? \
+        (static_assert((n) >= 0 && (n) <= 7, "Number of Long Addresses must be in range(0-7)"), \
+            (n << 4)) : \
+        (assert((n) >= 0 && (n) <= 7), \
+            (n << 4)))
+
+    // Bit 7 is Reserved
+
+//--------------------------------------------------------
+#pragma endregion Beacon Pending Address Specification Field
